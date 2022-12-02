fcf03ea63b0de5939345dd762115550c7c6213a8
nova777555 <62941854+nova777555@users.noreply.github.com>
2022-12-02 10:55:41 +0300

    Add files via upload

--- keyboards/new_appointment2.py
+++ keyboards/new_appointment2.py
@@ -5,7 +5,7 @@ import emoji
 kb_new_appointment2 = InlineKeyboardMarkup()
 
 i = 0
-for doctor in db_scripts.get_doctors():
+for doctor in db_scripts.get_doctors()[:-2]:
     kb_new_appointment2.add(InlineKeyboardButton(text = doctor[0] + ' (' + doctor[1] + ')', callback_data = 'choose_doctor_'+str(i+1)))
     i+=1
 kb_new_appointment2.add(InlineKeyboardButton('Назад ' + emoji.emojize(":arrow_left:", language='alias'),callback_data = 'back'))
